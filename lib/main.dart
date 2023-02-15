67f00f9514b0cc4e608b2e4d2d53f47a4c4496d5
Last-dreamer <asim0000.ak@gmail.com>
2023-02-15 23:10:10 +0500

    finilizing all the code

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Movie Recomenation App',
       darkTheme: CustomTheme.darkTheme(context),
       themeMode: ThemeMode.dark,
